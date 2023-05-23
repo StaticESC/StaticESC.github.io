befab0fe6937b9ac771720f20a082a5fadf607e4
Jyotirmay Shelly <jyotirma@ualberta.ca>
2023-05-22 19:29:39 -0600

    Added github and linkedin links with icon attributions, also got rid of WIP alert

--- main.js
+++ main.js
@@ -3,6 +3,3 @@ const myBday = new Date('2001-12-19');
 let age = new Date(Date.now() - myBday.getTime()).getUTCFullYear();
 age = Math.abs(age - 1970);
 document.getElementById('age').innerHTML = age;
-
-
-alert("This website is currently a WIP. Please check back later for more content.");
